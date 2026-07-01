f42b8e22151c0880c0528ec537968ce4dd836bde
UtsabGuha7 <utsabguha4@gmail.com>
2026-07-01 13:38:45 +0530

    0283-MOVE-ZEROES Stats: Time: 2 ms (92.02%), Space: 47.2 MB (97.75%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-      int n=nums.length;
-      int[]temp= new int[n];
-     int index=0;
-     for(int i=0;i<n;i++){
-        if(nums[i]!=0){
-        temp[index++]=nums[i];
+        int cnt=0;
+        int[] temp= new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                temp[cnt++]=nums[i];
+            }
         }
-     }
-     for(int i=0;i<n;i++){
-        nums[i]=temp[i];
-     }
+            for(int j=0;j<nums.length;j++){
+                nums[j]=temp[j];
+            }
     }
 }
