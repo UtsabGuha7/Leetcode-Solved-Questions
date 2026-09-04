7bcc66b991769960e018ce46f483a08777f575f2
Utsab Guha <utsabguha4@gmail.com>
2026-09-04 16:38:41 +0530

    0812-ROTATE-STRING Stats: Time: 2 ms (64.51%), Space: 43.6 MB (31.91%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,15 +1,6 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-       StringBuilder demo = new StringBuilder();
-        if(s.length()!=goal.length()){
-            return false;
-        }
-       for(int i=0;i<s.length();i++){
-        String join= s.substring(i)+s.substring(0,i);
-        if(join.equals(goal)){
-            return true;
-        }
-       } 
-       return false;
+        if(s.length()!=goal.length())  return false;
+           return (s+s).contains(goal);
     }
 }
