110a951240750606a0a6b0ee83c32f14b4acc084
Utsab Guha <utsabguha4@gmail.com>
2026-09-15 16:49:05 +0530

    0142-LINKED-LIST-CYCLE-II Stats: Time: 0 ms (100%), Space: 46.9 MB (13.58%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,15 +11,19 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        Map<ListNode,Integer> map = new HashMap<>();
-        ListNode temp=head;
-        while(temp!=null){
-            if(map.containsKey(temp)){
-                return temp;
-            }else{
-                map.put(temp,1);
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                slow=head;
+        while(slow!=fast){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        return slow;
             }
-            temp=temp.next;
         }
         return null;
     }
